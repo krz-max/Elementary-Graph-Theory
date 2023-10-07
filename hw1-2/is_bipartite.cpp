800b3ca5404a8fd492866307304c3c224b81acda
krz-max <a1314kevin@gmail.com>
2023-10-07 10:23:27 +0800

    HW1 Done

--- hw1-2/is_bipartite.cpp
+++ hw1-2/is_bipartite.cpp
@@ -3,45 +3,59 @@
 #include <queue>
 using namespace std;
 
-bool is_bipartite(vector<vector<int>>& graph, int start) {
-    vector<int> color(graph.size(), -1);
-    queue<pair<int, int>> q;
-    q.push({start, 0});
-    color[start] = 0;
+class Solution
+{
+public:
+    bool is_bipartite(const vector<vector<int>> &graph, int start)
+    {
+        vector<int> color(graph.size(), -1);
+        queue<pair<int, int>> q;
+        q.push({start, 0});
+        color[start] = 0;
 
-    while (!q.empty()) {
-        int node = q.front().first;
-        int c = q.front().second;
-        q.pop();
+        while (!q.empty())
+        {
+            int node = q.front().first;
+            int c = q.front().second;
+            q.pop();
 
-        for (int neighbor : graph[node]) {
-            if (color[neighbor] == -1) {
-                color[neighbor] = c ^ 1;
-                q.push({neighbor, c ^ 1});
-            } else if (color[neighbor] != (c ^ 1)) {
-                return false;
+            for (int neighbor : graph[node])
+            {
+                if (color[neighbor] == -1)
+                {
+                    color[neighbor] = (c == 0) ? 1 : 0;
+                    q.push({neighbor, color[neighbor]});
+                }
+                else if (color[neighbor] == c)
+                {
+                    return false;
+                }
             }
         }
+
+        return true;
     }
-
-    return true;
-}
-
-int main() {
+};
+int main()
+{
     int n, m;
     cin >> n >> m;
     vector<vector<int>> graph(n);
 
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; ++i)
+    {
         int a, b;
         cin >> a >> b;
         graph[a - 1].push_back(b - 1);
         graph[b - 1].push_back(a - 1);
     }
-
-    if (is_bipartite(graph, 0)) {
+    Solution Answer;
+    if (Answer.is_bipartite(graph, 0))
+    {
         cout << "Yes" << endl;
-    } else {
+    }
+    else
+    {
         cout << "No" << endl;
     }
 
